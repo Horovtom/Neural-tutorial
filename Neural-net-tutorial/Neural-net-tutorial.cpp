c77bafb9a7da25f67813d3cbb3f057ee6975a088
Tomáš Hořovský <horovtom@fel.cvut.cz>
2016-09-26 19:48:53 +0200

    Test run

--- Neural-net-tutorial/Neural-net-tutorial.cpp
+++ Neural-net-tutorial/Neural-net-tutorial.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include <vector>
 #include <iostream>
+#include <stdio.h>
 
 class Neuron {};
 typedef std::vector<Neuron> Layer;
@@ -22,7 +23,7 @@ private:
 
 //Define the Net constructor:
 Net::Net(const std::vector<unsigned> &topology) {
-	unsigned numLayers = topology.size;
+	unsigned numLayers = topology.size();
 	for (unsigned layerNum = 0; layerNum < numLayers; ++layerNum) {
 		//append
 		m_layers.push_back(Layer());
@@ -31,6 +32,7 @@ Net::Net(const std::vector<unsigned> &topology) {
 		for (unsigned neuronNum = 0; neuronNum < topology[layerNum]; ++neuronNum) {
 			//get last element in m_layers (Layer) and append a new neuron to it.
 			m_layers.back().push_back(Neuron());
+			std::cout << "Made a Neuron!" << std::endl;
 		}
 
 	}
@@ -41,6 +43,10 @@ int main()
 {
 	// e.g., {3, 2, 1} (3 layers, 1st - 3 nodes, 2nd - 2 nodes, 3rd - 1 node
 	std::vector<unsigned> topology;
+	topology.push_back(3);
+	topology.push_back(2);
+	topology.push_back(1);
+
 	//Construct
 	Net myNet(topology);
 
