716e8fdc76358801082301e3881139b286c053bc
Tomáš Hořovský <tomashorovsky@seznam.cz>
2016-09-26 22:23:46 +0200

    Implementing new input mechanism

--- Neural-net-tutorial/Neural-net-tutorial.cpp
+++ Neural-net-tutorial/Neural-net-tutorial.cpp
@@ -8,6 +8,85 @@
 #include <cstdlib>
 #include <cassert>
 #include <cmath>
+#include <fstream>
+#include <sstream>
+
+
+class TrainingData{
+public:
+    TrainingData(const std::string filename);
+    bool isEof(void) { return m_trainingDataFile.eof();}
+    void getTopology(std::vector<unsigned> &topology);
+
+    unsigned getNextInputs(std::vector<double> &inputVals);
+    unsigned getTargetOutputs(std::vector<double> &targetOutputVals);
+
+
+private:
+    std::ifstream m_trainingDataFile;
+};
+
+void TrainingData::getTopology(std::vector<unsigned> &topology) {
+    std::string line;
+    std::string label;
+
+    std::getline(m_trainingDataFile, line);
+    std::stringstream ss(line);
+    ss >> label;
+    if (this->isEof() || label.compare("topology:") != 0){
+        abort();
+    }
+
+    while (!ss.eof()){
+        unsigned n;
+        ss >> n;
+        topology.push_back(n);
+    }
+
+    return;
+}
+
+TrainingData::TrainingData(const std::string filename) {
+    m_trainingDataFile.open(filename.c_str());
+}
+
+unsigned TrainingData::getNextInputs(std::vector<double> &inputVals) {
+    inputVals.clear();
+
+    std::string line;
+    getline(m_trainingDataFile, line);
+    std::stringstream ss(line);
+
+    std::string label;
+    ss >> label;
+
+    if (label.compare("in:") == 0){
+        double oneValue;
+        while (ss >> oneValue){
+            inputVals.push_back(oneValue);
+        }
+    }
+
+    return inputVals.size();
+}
+
+unsigned TrainingData::getTargetOutputs(std::vector<double> &targetOutputVals) {
+    targetOutputVals.clear();
+
+    std::string line;
+    getline(m_trainingDataFile, line);
+    std::stringstream ss(line);
+
+    std::string label;
+    ss >> label;
+    if (label.compare("out:") == 0){
+        double oneValue;
+        while(ss>>oneValue){
+            targetOutputVals.push_back(oneValue);
+        }
+    }
+    return targetOutputVals.size();
+}
 
 struct Connection {
     double weight, deltaWeight;
@@ -22,11 +101,17 @@ typedef std::vector<Neuron> Layer;
 class Neuron {
 public:
     Neuron(unsigned numOutputs, unsigned myIndex);
+
     void feedForward(const Layer &prevLayer);
+
     void calcOutputGradients(double targetVal);
+
     void calcHiddenGradients(const Layer &nextLayer);
+
     void updateInputWeights(Layer &prevLayer);
+
     void setOutputVal(const double val) { m_outputVal = val; }
+
     double getOutputVal(void) const { return m_outputVal; }
 
 private:
@@ -34,11 +119,13 @@ private:
     static double alpha; //[0,0 ... 1,0] multiplier of last weight change (momentum)
     static double randomWeight(void) { return rand() / double(RAND_MAX); } //This returns random value between 0 and 1
     static double activationFunction(double x);
+
     static double activationFunctionDerivative(double x);
+
     double sumDOW(const Layer &nextLayer) const;
 
     double m_outputVal;
-    double m_bias;
+    double m_bias = 1;
     std::vector<Connection> m_outputWeights;
     unsigned m_myIndex;
     double m_gradient;
@@ -62,7 +149,7 @@ void Neuron::updateInputWeights(Layer &prevLayer) {
                 * m_gradient
                 //Also add momentum = a fraction of the previous delta weight
                 + alpha
-                * oldDeltaWeight;
+                  * oldDeltaWeight;
 
         neuron.m_outputWeights[m_myIndex].deltaWeight = newDeltaWeight;
         neuron.m_outputWeights[m_myIndex].weight += newDeltaWeight;
@@ -136,6 +223,7 @@ public:
     //getResults(resultVals) is a function that only reads the result, does not modify the Net object at all. Thats why its const
     void getResults(std::vector<double> &resultVals) const;
 
+    double getRecentAverageError(void) const {return m_recentAverageError;}
 private:
     std::vector<Layer> m_layers; //m_layers[layerNum][neuronNum]
     double m_error;
@@ -165,7 +253,8 @@ void Net::backProp(const std::vector<double> &targetVals) {
 
     //Implement a recent average measurement:
 
-    m_recentAverageError = (m_recentAverageError * m_recentAverageSmoothingFactor + m_error) / (m_recentAverageSmoothingFactor + 1.0);
+    m_recentAverageError =
+            (m_recentAverageError * m_recentAverageSmoothingFactor + m_error) / (m_recentAverageSmoothingFactor + 1.0);
 
     //Calculate output layer gradients
     //1: The last layer (output layer)
@@ -173,7 +262,7 @@ void Net::backProp(const std::vector<double> &targetVals) {
         outputLayer[n].calcOutputGradients(targetVals[n]);
     }
     //2: Other layers
-    for (int layerNum = m_layers.size() - 2; layerNum > 0 ; --layerNum) {
+    for (int layerNum = m_layers.size() - 2; layerNum > 0; --layerNum) {
         Layer &currentLayer = m_layers[layerNum];
         Layer &nextLayer = m_layers[layerNum + 1];
 
@@ -181,7 +270,7 @@ void Net::backProp(const std::vector<double> &targetVals) {
             currentLayer[n].calcHiddenGradients(nextLayer);
         }
     }
-    
+
     //For all layers from outputs to first hidden layer update connection weights
     for (unsigned layerNum = m_layers.size() - 1; layerNum > 0; --layerNum) {
         Layer &layer = m_layers[layerNum];
@@ -191,7 +280,7 @@ void Net::backProp(const std::vector<double> &targetVals) {
             layer[n].updateInputWeights(prevLayer);
         }
     }
-    
+
 }
 
 void Net::feedForward(const std::vector<double> &inputVals) {
@@ -224,33 +313,80 @@ Net::Net(const std::vector<unsigned> &topology) {
 
             //get last element in m_layers (Layer) and append a new neuron to it.
             m_layers.back().push_back(Neuron(numOutputs, neuronNum));
-            std::cout << "Made a Neuron!" << std::endl;
+            //std::cout << "Made a Neuron!" << std::endl;
         }
 
     }
 
 }
 
-int main() {
-    // e.g., {3, 2, 1} (3 layers, 1st - 3 nodes, 2nd - 2 nodes, 3rd - 1 node
-    std::vector<unsigned> topology;
-    topology.push_back(3);
-    topology.push_back(2);
-    topology.push_back(1);
+//Dummy main:
+//int main() {
+//    // e.g., {3, 2, 1} (3 layers, 1st - 3 nodes, 2nd - 2 nodes, 3rd - 1 node
+//    std::vector<unsigned> topology;
+//    topology.push_back(3);
+//    topology.push_back(2);
+//    topology.push_back(1);
+//
+//    //Construct
+//    Net myNet(topology);
+//
+//    std::vector<double> inputVals;
+//    //Train
+//    myNet.feedForward(inputVals);
+//
+//    std::vector<double> targetVals;
+//    myNet.backProp(targetVals);
+//    //Save results
+//
+//    std::vector<double> resultVals;
+//    myNet.getResults(resultVals);
+//
+//}
 
-    //Construct
+void showVectorVals(std::string label, std::vector<double> &v) {
+    std::cout << label << " ";
+    for (unsigned i = 0; i < v.size(); ++i) {
+        std::cout << v[i] << " ";
+    }
+
+    std::cout << std::endl;
+}
+
+int main() {
+    TrainingData trainData("/tmp/trainingData.txt");
+
+    std::vector<unsigned> topology;
+    trainData.getTopology(topology);
     Net myNet(topology);
 
-    std::vector<double> inputVals;
-    //Train
-    myNet.feedForward(inputVals);
+    std::vector<double> inputVals, targetVals, resultVals;
+    int trainingPass = 0;
 
-    std::vector<double> targetVals;
-    myNet.backProp(targetVals);
-    //Save results
+    while (!trainData.isEof()) {
+        ++trainingPass;
+        std::cout << std::endl << "Pass " << trainingPass;
 
-    std::vector<double> resultVals;
-    myNet.getResults(resultVals);
+        //Get new input data and feed it forward:
+        if (trainData.getNextInputs(inputVals) != topology[0]) break;
+        showVectorVals(": Inputs:", inputVals);
+        myNet.feedForward(inputVals);
 
+        //Collect the net´s actual output results:
+        myNet.getResults(resultVals);
+        showVectorVals("Outputs:", resultVals);
+
+        //Train the net what the outputs should have been:
+        trainData.getTargetOutputs(targetVals);
+        showVectorVals("Targets:", targetVals);
+        assert(targetVals.size() == topology.back());
+
+        myNet.backProp(targetVals);
+
+        //Report how well the training is working, average over recent samples:
+        std::cout < "Net recent average error: " << myNet.getRecentAverageError() << std::endl;
+    }
+
+    std::cout << std::endl << "Done" << std::endl;
 }
 
